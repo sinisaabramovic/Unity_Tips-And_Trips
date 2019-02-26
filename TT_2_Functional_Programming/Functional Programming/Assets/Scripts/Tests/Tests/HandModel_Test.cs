37de1a3448079bd9dc54d38b694d8413614f6153
Sinisa Abramovic <sinisa.abramovich@gmail.com>
2019-02-26 20:00:57 +0100

    - added tests for handmodel to check conditions for flush and royal flush

--- TT_2_Functional_Programming/Functional Programming/Assets/Scripts/Tests/Tests/HandModel_Test.cs
+++ TT_2_Functional_Programming/Functional Programming/Assets/Scripts/Tests/Tests/HandModel_Test.cs
@@ -57,6 +57,32 @@ public class HandModel_Test {
         Assert.AreEqual(CardValue.King, hand.HighCard().Value);
     }
 
+    [Test]
+    public void CanScoreAFlush()
+    {
+        HandModel hand = new HandModel();
+        hand.Draw(new CardModel(CardValue.Seven, CardSuit.Hearts));
+        hand.Draw(new CardModel(CardValue.Ten, CardSuit.Hearts));
+        hand.Draw(new CardModel(CardValue.Five, CardSuit.Hearts));
+        hand.Draw(new CardModel(CardValue.King, CardSuit.Hearts));
+        hand.Draw(new CardModel(CardValue.Two, CardSuit.Hearts));
+
+        Assert.AreEqual(HandRank.Flush, hand.GetHandRank());
+    }
+
+    [Test]
+    public void CanScoreARoyalFlush()
+    {
+        HandModel hand = new HandModel();
+        hand.Draw(new CardModel(CardValue.Ten, CardSuit.Hearts));
+        hand.Draw(new CardModel(CardValue.Jack, CardSuit.Hearts));
+        hand.Draw(new CardModel(CardValue.Queen, CardSuit.Hearts));
+        hand.Draw(new CardModel(CardValue.King, CardSuit.Hearts));
+        hand.Draw(new CardModel(CardValue.Ace, CardSuit.Hearts));
+
+        Assert.AreEqual(HandRank.RoyalFlush, hand.GetHandRank());
+    }
+
     // A UnityTest behaves like a coroutine in PlayMode
     // and allows you to yield null to skip a frame in EditMode
     [UnityTest]
