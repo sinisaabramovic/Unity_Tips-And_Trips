55a01f21c4aad9a93aedf4920ae5431e47422417
Sinisa Abramovic <sinisa.abramovich@gmail.com>
2019-02-26 19:47:34 +0100

    Added test for check is a two card models are equal or not equal

--- TT_2_Functional_Programming/Functional Programming/Assets/Scripts/Tests/Tests/CardModel_Test.cs
+++ TT_2_Functional_Programming/Functional Programming/Assets/Scripts/Tests/Tests/CardModel_Test.cs
@@ -23,6 +23,24 @@ public class CardModel_Test {
         Assert.AreEqual("Ace of Clubs", cardModel.ToString());
     }
 
+    [Test]
+    public void IsEqualTest()
+    {
+        CardModel cardModel_1 = new CardModel(CardValue.Ace, CardSuit.Clubs);
+        CardModel cardModel_2 = new CardModel(CardValue.Ace, CardSuit.Clubs);
+
+        Assert.True(cardModel_1.isEqual(cardModel_2), "Two card objects should be equal!");
+    }
+
+    [Test]
+    public void IsNotEqualTest()
+    {
+        CardModel cardModel_1 = new CardModel(CardValue.Ace, CardSuit.Clubs);
+        CardModel cardModel_2 = new CardModel(CardValue.Ace, CardSuit.Hearts);
+
+        Assert.False(cardModel_1.isEqual(cardModel_2), "Two card objects should not be equal!");
+    }
+
     // A UnityTest behaves like a coroutine in PlayMode
     // and allows you to yield null to skip a frame in EditMode
     [UnityTest]
